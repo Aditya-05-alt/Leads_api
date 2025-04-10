f4be5b55bd6f4a9d021743de10c38fb53cc544f2
Aditya-05-alt <113160241+Aditya-05-alt@users.noreply.github.com>
2025-04-10 15:44:59 +0530

    Update form_tracker.js

--- form_tracker.js
+++ form_tracker.js
@@ -35,7 +35,7 @@
         }
 
         // Send data to Django API
-        fetch("https://leadtracker-production.up.railway.app/api/leads/create/", {
+        fetch("https://leadtracker-production.up.railway.app/api/leads/create/"{
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formDetails),
